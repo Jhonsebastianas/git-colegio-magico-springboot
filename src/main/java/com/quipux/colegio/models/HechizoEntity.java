9717a58e10b026cea0ba9ae30fe7ec65355be4e8
montoya-nmc <olcenico578@gmail.com>
2026-09-22 14:25:01 -0500

    Feat: Reto 1 resuelto

--- src/main/java/com/quipux/colegio/models/HechizoEntity.java
+++ src/main/java/com/quipux/colegio/models/HechizoEntity.java
@@ -1,4 +1,10 @@
 package com.quipux.colegio.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 // RETO 1: Agrega las anotaciones necesarias para que esta clase sea una Entidad de Base de Datos.
 // PISTAS: 
@@ -7,12 +13,20 @@ package com.quipux.colegio.models;
 // 3. El atributo 'id' debe ser la llave primaria (@I...) y autogenerada (@G...)
 // 4. El atributo 'nombre' debe mapearse a una columna (@C...) y no debe permitir nulos (nullable = false).
 
+@Entity
+@Table(name = "hechizos")
 public class HechizoEntity {
-    
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable = false)
     private String nombre;
     private String tipoMagia; // Ejemplo: Fuego, Agua, Oscura
+     
+    
     private Integer nivelPoder;
     
     // Getters y Setters
