48833b7f31d7bdc3c28a5efc9c522c78c6ead542
giraldoalzatehaslysofia-hash <giraldoalzatehaslysofia@gmail.com>
2026-09-23 01:26:52 +0000

    feat:se actualizo @Transactional agregando rollbackFor = Exception.class en HechizoManagerImpl para garantizar el rollback automatico ante excepciones de negocio

--- src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
+++ src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
@@ -8,12 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-// RETO 3: Agrega las anotaciones de Spring Boot para que esta clase sea un Servicio de Lógica
-// y para que los métodos se ejecuten dentro de una transacción de base de datos.
-// PISTAS: @S... y @T...
-
+// RETO 3: Agrega las anotaciones de Spring Boot
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public class HechizoManagerImpl implements HechizoManager {
 
     @Autowired
@@ -23,10 +20,6 @@ public class HechizoManagerImpl implements HechizoManager {
     public HechizoEntity registrarHechizo(HechizoEntity hechizo) throws Exception {
         
         // RETO 3.1: Validar Reglas Mágicas
-        // 1. Si el nombre del hechizo es nulo o está vacío, debes lanzar una Exception con el mensaje "Nombre invalido".
-        // 2. Si el "tipoMagia" del hechizo es "Oscura", debes lanzar una Exception con el mensaje "Magia prohibida en el colegio".
-        
-        // Escribe tu código aquí:
         if (hechizo == null || hechizo.getNombre() == null || hechizo.getNombre().trim().isEmpty()) {
             throw new Exception("Nombre invalido");
         }
