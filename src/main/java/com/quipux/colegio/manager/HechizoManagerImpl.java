ebaf0ba32c17c4311af28beaa3b8271ef8e1ae20
Andres Valencia <pipevalenciamena5@gmail.com>
2026-09-25 00:13:44 -0500

    feat:Reto 3 completado Andres Valencia

--- src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
+++ src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
@@ -11,7 +11,8 @@ import java.util.List;
 // RETO 3: Agrega las anotaciones de Spring Boot para que esta clase sea un Servicio de Lógica
 // y para que los métodos se ejecuten dentro de una transacción de base de datos.
 // PISTAS: @S... y @T...
-
+@Service 
+@Transactional
 public class HechizoManagerImpl implements HechizoManager {
 
     @Autowired
@@ -23,9 +24,14 @@ public class HechizoManagerImpl implements HechizoManager {
         // 1. Si el nombre del hechizo es nulo o está vacío, debes lanzar una Exception con el mensaje "Nombre invalido".
         // 2. Si el "tipoMagia" del hechizo es "Oscura", debes lanzar una Exception con el mensaje "Magia prohibida en el colegio".
         
-        // Escribe tu código aquí:
+        if (hechizo.getNombre() == null || hechizo.getNombre().isEmpty()) {
+            throw new Exception("Nombre invalido");
+        }
         
-        
+        if ("Oscura".equals(hechizo.getTipoMagia())) {
+            throw new Exception("Magia prohibida en el colegio");
+        }
+
         return hechizoDao.guardarHechizo(hechizo);
     }
 
