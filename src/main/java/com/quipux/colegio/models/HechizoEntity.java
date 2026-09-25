cba2617da1c10b1740af2da96d054d238b432f94
Capigamer888 <junior.capi2010@gmail.com>
2026-09-25 21:52:27 +0000

    Añadir validaciones y restricciones a columnas a HechizoEntity

--- src/main/java/com/quipux/colegio/models/HechizoEntity.java
+++ src/main/java/com/quipux/colegio/models/HechizoEntity.java
@@ -1,5 +1,12 @@
 package com.quipux.colegio.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 // RETO 1: Agrega las anotaciones necesarias para que esta clase sea una Entidad de Base de Datos.
 // PISTAS: 
 // 1. Necesitas marcar la clase con @E...
@@ -7,12 +14,25 @@ package com.quipux.colegio.models;
 // 3. El atributo 'id' debe ser la llave primaria (@I...) y autogenerada (@G...)
 // 4. El atributo 'nombre' debe mapearse a una columna (@C...) y no debe permitir nulos (nullable = false).
 
+@Entity 
+@Table(name = "hechizos") 
 public class HechizoEntity {
     
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
     
+
+    @Column(name="nombre", nullable = false, length = 80, unique = true) 
     private String nombre;
-    private String tipoMagia; // Ejemplo: Fuego, Agua, Oscura
+
+
+    public enum tipoMagia {
+        Fuego, Agua, Oscura    
+    }
+    @Column(name="tipo_magia", nullable = false, length = 20) 
+    private String tipoMagia;
+
+    @Column(name="nivel_poder", nullable = false)
     private Integer nivelPoder;
     
     // Getters y Setters
