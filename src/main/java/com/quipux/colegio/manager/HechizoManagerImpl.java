00a103bb06e698e3e31e71057d2116a70d54ac70
salazar1014 <emsalazar755@gmail.com>
2026-09-26 02:05:23 +0000

    feat: se agrega el @service y el @transactional, en el service va la logica del programa y en el transactional hace que las operaciones de la base de datos se ejecuten dentro de la transaccion y por ultimo se completa el if para validar que en el registrarHechizo no sea nulo ni este vacio

--- src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
+++ src/main/java/com/quipux/colegio/manager/HechizoManagerImpl.java
@@ -12,6 +12,8 @@ import java.util.List;
 // y para que los métodos se ejecuten dentro de una transacción de base de datos.
 // PISTAS: @S... y @T...
 
+@Service
+@Transactional
 public class HechizoManagerImpl implements HechizoManager {
 
     @Autowired
@@ -19,13 +21,19 @@ public class HechizoManagerImpl implements HechizoManager {
 
     @Override
     public HechizoEntity registrarHechizo(HechizoEntity hechizo) throws Exception {
+
         // RETO 3.1: Validar Reglas Mágicas
-        // 1. Si el nombre del hechizo es nulo o está vacío, debes lanzar una Exception con el mensaje "Nombre invalido".
-        // 2. Si el "tipoMagia" del hechizo es "Oscura", debes lanzar una Exception con el mensaje "Magia prohibida en el colegio".
-        
-        // Escribe tu código aquí:
-        
-        
+
+        // 1. Si el nombre del hechizo es nulo o está vacío
+        if (hechizo.getNombre() == null || hechizo.getNombre().trim().isEmpty()) {
+            throw new Exception("Nombre invalido");
+        }
+
+        // 2. Si el tipo de magia es Oscura
+        if ("Oscura".equals(hechizo.getTipoMagia())) {
+            throw new Exception("Magia prohibida en el colegio");
+        }
+
         return hechizoDao.guardarHechizo(hechizo);
     }
 
